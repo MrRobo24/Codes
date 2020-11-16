e5ae8f63d5e2101bb3fdbebc2c32cb98995c7b7d
MrRobo24 <sumanarpit.24@gmail.com>
2020-11-16 14:30:19 +0530

    Long Challenge Nov 2020 Ended

--- CC/CChef/7_UNSQUERS.cpp
+++ CC/CChef/7_UNSQUERS.cpp
@@ -4,119 +4,29 @@
 using namespace std;
 LLI n, q, s, gMax = INT_MIN;
 LLI arr[100000];
-LLI critical[100000];
-vector< vector<LLI> > forest;
-
-LLI calcDepth(LLI l, LLI r) {
-    if (r < l ) {
-        return 0;
-    }
-
-    LLI maxim = 0;
-
-    for (LLI i=0;i<forest[r].size();i++) {
-        LLI depth = calcDepth(l, forest[r][i]);
-        maxim = max(depth, maxim);
-    }
-    
-    return maxim + 1;
-}
-
 
 LLI solve(LLI l, LLI r) {
-    //finding maxim
-    //cout << "l & r " << l << " " << r << "\n";    
-    LLI ans = INT_MIN, curr = 1;
+    LLI curr = 1, ans = INT_MIN;
+    //cout << l << " " << r << "\n";
+    for (LLI i=l;i<=r;i++) {
+        LLI prev = arr[i];
+        curr = 1;
+        for (LLI j=i+1;j<=r;j++) {
+            if (arr[j] > prev) {
+                curr++;
+                prev = arr[j];
+            }
 
-    LLI i = r;
-    while (i >= l) {
-        if (!forest[i].empty()) {
-            curr = calcDepth(l, i);
-            break;
+            if (arr[j] == gMax && j+1 <= r) {
+                prev = arr[j+1];
+                ans = max(curr, ans);
+                curr = 1;
+            }
         }
-        i--;
-    }
-    
-    //cout << "CURR: " << curr << "\n";
-    ans = max(ans, curr);
-
-    
-
-    while (i >= l && critical[i] != -1 && critical[i] >= l) {
-
-        if (critical[i] != r) {
-            //cout << "CALLING FOR " << critical[i] << "\n";
-            curr = calcDepth(l, critical[i]);
-            //cout << "CURR: " << curr << "\n";
-            ans = max(ans, curr);
-        }
-        i--;
+        ans = max(curr, ans);
     }
 
     return ans;
-}
-
-void storeCriticalPoints() {
-    LLI curr = -1;;
-    for (LLI i=0;i<n;i++) {
-        if (arr[i] == gMax) {
-            critical[i] = i;
-            curr = i;
-        } else {
-            critical[i] = curr;
-        }
-    }
-
-    // cout << "CRITICAL POINTS: ";
-    // for (LLI i=0;i<n;i++) {
-    //     cout << critical[i] << " ";
-    // }
-    // cout << "\n";
-
-}
-
-void makeForest() {
-    vector <LLI> peaks;
-    LLI prev = arr[0];
-    for (LLI i=1;i<n;i++) {
-        if (arr[i] > prev) {
-            peaks.push_back(i);
-        }
-        prev = arr[i];
-    }
-
-    // cout << "PEAKS: ";
-    // for (LLI i=0;i<peaks.size();i++) {
-    //     cout << peaks[i] << " ";
-    // }
-    // cout << "\n";
-
-    
-    forest.resize(n);
-    for (LLI i=0;i<n-1;i++) {
-        if (arr[i] < arr[i+1]) {
-            forest[i+1].push_back(i);
-        } else {
-            LLI j = 0, s = peaks.size();
-            while (j < s) {
-                if (arr[peaks[j]] > arr[i]) {
-                    forest[peaks[j]].push_back(i);
-                    break;
-                }
-                j++;
-            }
-        }
-    }
-
-    // cout << "FOREST \n";
-    // for (LLI i=0;i<n;i++) {
-    //     cout << i << " -> ";
-    //     for (LLI j=0;j<forest[i].size();j++) {
-    //         cout << forest[i][j] << " ";
-    //     }
-    //     cout << "\n";
-
-    // }
 }
 
 
@@ -128,11 +38,10 @@ int main() {
     cin >> n >> q >> s;
     for (LLI i=0;i<n;i++) {
         cin >> arr[i];
-        // critical[i] = 0;
+        gMax = max(arr[i], gMax);
     }
-    gMax = *max_element(arr, arr+n);
-    storeCriticalPoints();
-    makeForest();
+
+    //gMax = *max_element(arr, arr+n);
 
     LLI ans = 0;
     while (q--) {
